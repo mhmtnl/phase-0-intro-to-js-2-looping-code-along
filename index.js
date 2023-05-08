6d8013320c532f99d9d07188bd4d0bd444790a39
mehmetunlu <munlu4037@gmail.com>
2023-05-08 16:46:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+
+let arr = [];
+function writeCards(groupName, reason){
+    groupName.forEach(element => {
+        arr.push(`Thank you, ${element}, for the wonderful ${reason} gift!`);
+    })
+    return arr;
+}
+
+function countDown(num){
+    while (0 <= num){
+        console.log(num);
+        num--;
+    }
+}
